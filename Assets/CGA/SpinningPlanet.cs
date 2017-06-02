a7d89ee61b099fe8d105d4b41d504e2b422c9892
paulsmithkc <paulsmithkc@gmail.com>
2017-06-02 00:16:28 -0500

    Messing around with image effects

---
+++ Assets/CGA/SpinningPlanet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningPlanet : MonoBehaviour {
+
+    public float rotationSpeed = 0;
+    public Vector3 axis = Vector3.up;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(axis, rotationSpeed * Time.deltaTime);
+	}
+}

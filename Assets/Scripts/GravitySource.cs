67d56fdfd93a3fdd0417a2590bbef3674026479e
Tyler Funk <tfunk@fastmail.fm>
2017-06-02 00:12:23 -0500

    GravitySource is now super-simplified

--- Assets/Scripts/GravitySource.cs
+++ Assets/Scripts/GravitySource.cs
@@ -6,46 +6,18 @@ public class GravitySource : MonoBehaviour
 {
 
     public float gravityScale;
-    public float gravityRadius;
 
-    private float radius;
-    private float sqRadius;
-
-	// Use this for initialization
-	void Start ()
+    private void OnTriggerStay(Collider other)
     {
-        UpdateSqRadius();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
-    {
-        // Get a list of game objects within gravity radius which have rigidbodies
-        Attractable[] list = FindObjectsOfType<Attractable>();
-        for (var i = 0; i < list.Length; i++)
+        // If the object has a rigidbody...
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        if (rb)
         {
-            // If the object has a rigidbody...
-            Rigidbody rb = list[i].GetComponent<Rigidbody>();
-            if (rb)
-            {
-                // And is in range...
-                Vector3 dist = transform.position - rb.transform.position;
-                if (dist.sqrMagnitude <= sqRadius)
-                {
-                    // Apply gravity!
-                    Vector3 force = dist.normalized * gravityScale * list[i].gravityScale;
-                    rb.AddForce(force, ForceMode.Acceleration);
-                }
-            }
-        }
-	}
-
-    void UpdateSqRadius()
-    {
-        if (radius != gravityRadius)
-        {
-            radius = gravityRadius;
-            sqRadius = radius * radius;
+            // Apply gravity!
+            Vector3 dist = transform.position - other.transform.position;
+            Vector3 force = dist.normalized * gravityScale;
+            rb.AddForce(force, ForceMode.Acceleration);
         }
     }
+
 }

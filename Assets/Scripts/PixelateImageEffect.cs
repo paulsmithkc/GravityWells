f68c5448b754c2fcf8d0a102bcb5fc6c8991e79a
paulsmithkc <paulsmithkc@gmail.com>
2017-06-05 01:10:44 -0500

    Playing aeound with image effects

---
+++ Assets/Scripts/PixelateImageEffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelateImageEffect : MonoBehaviour {
+    
+    [Range(0, 7)]
+    public int downsample = 1;
+    
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        RenderTexture t = RenderTexture.GetTemporary(src.width >> downsample, src.height >> downsample);
+        Graphics.Blit(src, t);
+        Graphics.Blit(t, dest);
+        RenderTexture.ReleaseTemporary(t);
+    }
+}

424c6f6f94225851774fab3cd918a7a4a7d86b71
merlin4 <paulsmithkc@gmail.com>
2017-06-09 23:02:08 -0500

    Adding random spread to the rocket launcher.

--- Assets/Scripts/RocketLauncher.cs
+++ Assets/Scripts/RocketLauncher.cs
@@ -6,13 +6,18 @@ public class RocketLauncher : MonoBehaviour {
     
     public Rocket rocketPrefab;
     public string fireButton = "Fire1";
+    public float spreadFactor = 1;
     
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown(fireButton)) {
+            var r = Random.insideUnitCircle * spreadFactor;
             GameObject.Instantiate(
                 rocketPrefab,
-                transform.position + transform.forward * rocketPrefab.initialSpeed * 0.15f,
+                transform.position 
+                  + transform.forward * rocketPrefab.initialSpeed * 0.15f 
+                  + transform.right * r.x
+                  + transform.up * r.y,
                 transform.rotation
             );
         }
